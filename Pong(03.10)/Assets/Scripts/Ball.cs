5136d3565427eab26c949cd8182c68e7ed883065
lunandhip <krezitim777@mail.ru>
2021-10-03 16:45:17 +0300

    Update Ball.cs

--- Pong(03.10)/Assets/Scripts/Ball.cs
+++ Pong(03.10)/Assets/Scripts/Ball.cs
@@ -1,29 +1,29 @@
-﻿using System.Collections;// Делает хуй знает что
-using System.Collections.Generic;// Делает хуй знает что
-using UnityEngine; // Делает хуй знает что
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Ball : MonoBehaviour 
 {
-    public Rigidbody2D regidbody; // Получаем доступ к телу. Опять же создаю переменную
+    public Rigidbody2D regidbody; // Получаем доступ к телу
     public float speed; //Создаем переменую для скорости
     public Vector2 direction; // Вектор для задания направления шарика
-    public float baseSpeed; //Переменная для ускорения, храми базлвую скоорость.
+    public float baseSpeed; //Переменная для ускорения, храми базовую скоорость.
     public float coefficientSpeed; // Множитель скорости
-    public int points; //Хранит чмсло ударов об ракетку.Можно прихуячить еще одну пременнную которая будет показывать 
+    public int points; //Хранит число ударов об ракетку
     public Player player; // создаем ссылку на компонет плеер
     int score1;
     int score2;
     int Score_Player; // Переменная для подсчета очков
     int funal; // Переменная считающая когда игра все
     public static bool paused = false; // Переменная для паузы
-    public GameObject panel;// Переменная для паузы
+    public GameObject panel;// Кнопка для паузы
 
     // Start is called before the first frame update
-    void Start() //срабатывает 1 раз при старте игры. Если конечно незахоти друго-го
+    void Start()
     {
         transform.position = Vector2.zero;
         //transform.position = Vector2.zero; // задаем начальное положение чтобы когда шарик улете за грань он снова появился здесь
-        direction = new Vector2(Random.Range(-0.5f, 0.7f), Random.Range(-0.7f,0.5f)); // Задаем рандомное нарпавление для старта шарика. Он потетитт в рандомном наравлении продикторванные x и y. f дает понять юинит что используетсья флоат число. Зачем вставлять еще один Random.Range(0.5f,1))? Это генериет значение дял другйо оси?
+        direction = new Vector2(Random.Range(-0.5f, 0.7f), Random.Range(-0.7f,0.5f)); // Задаем рандомное нарпавление для старта шарика. Он потетитт в рандомном наравлении продикторванные x и y. f дает понять юинит что используетсья флоат число.
         //direction = new Vector2(Random.Range(-0.5f, 1), Random.Range(0.5f, -1));
         //speed = baseSpeed;
         speed = 5;
@@ -52,22 +52,18 @@ public class Ball : MonoBehaviour
         
             }
         }
-        regidbody.velocity = direction.normalized * speed; // .normalized нужен для нормализации вектора? Чтобы не выходил за границы возможного числового значения
-        //if (transform.position.x > player.transform.position.x + 1) // если позиция шарика больше чем у платформы то она 
-        //{
-        //    Debug.Log("Вы проиграли");
-        //    Start(); // ЗАпускаем игру повторно
-    
+        regidbody.velocity = direction.normalized * speed; // Чтобы не выходил за границы возможного числового значения
+        
     }
     private void OnCollisionEnter2D(Collision2D col) // Срабатывает когда обьект с бокс коллидер сталкиваеться с другим кбокс коллидеро col-переменная куда это пережаеться
     {
-        if (col.gameObject.CompareTag("Player")) // Здесь Player это название ТЕГА! как его выбрать есть в треле
+        if (col.gameObject.CompareTag("Player")) // Здесь Player это название тега
         {
-            direction.x = -direction.x; // когда шарик соприкоснеться с ракеткой он изменит свое знгачение х на противоположное. Там же минус ага
+            direction.x = -direction.x; // когда шарик соприкоснеться с ракеткой он изменит свое знгачение х на противоположное
             speed = speed * coefficientSpeed; // при соприкосновении с ракеткой скорсоть мячика возрастает. Скорость умножаеться на множитель который мы тоже указываем с клавиатуры
             //points = points + 1; 
             points++;
-            //Debug.Log("Вы набрали - " + points + " очков"); // Я модифицировал окно. <-Что это вообще значит?
+            
         }
         if (col.gameObject.CompareTag("Respawn")) // Правая палочка твикс
         {
@@ -76,9 +72,9 @@ public class Ball : MonoBehaviour
             score1++;
             Debug.Log("Белый игрок- "+score1);
             Score.scoreAmount += 1; // Переменная для подсчета очков
-            Start(); // Чтобы игра началасб заново. Мы вызываем срабатывание блока старт?.Если убрать его то шарик не перещаетьсян а нуливую
+            Start(); // Чтобы игра началась заново. Мы вызываем срабатывание блока старт.
         }
-        if (col.gameObject.CompareTag("Respawn2"))
+        if (col.gameObject.CompareTag("Respawn2"))// Левая палочка твикс
         {
             //transform.position = Vector2.zero;
             score2++;
